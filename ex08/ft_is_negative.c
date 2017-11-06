bc84d9e1dd582a03c138af4e4c945262ffc67f94
Audrey LAMY <alamy@e1r11p15.42.fr>
2017-11-06 19:35:36 +0100

    first commit

---
+++ ex08/ft_is_negative.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alamy <alamy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/06 18:20:31 by alamy             #+#    #+#             */
+/*   Updated: 2017/11/06 18:38:43 by alamy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('N');
+	}
+	else if (n >= 0)
+	{
+		ft_putchar('P');
+	}
+}
+
+int		main(void)
+{
+	ft_is_negative(-100);
+	ft_is_negative(-1);
+	ft_is_negative(0);
+	ft_is_negative(20);
+	return (0);
+}
